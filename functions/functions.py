6cebd67f29f50a2f819fc3b004ba323636b86f0c
Simon <ndungusimon564@gmail.com>
2025-09-26 14:29:57 +0300

    learning python

---
+++ functions/functions.py
@@ -0,0 +1,10 @@
+## Functions= block of code that performs sequence of actions when called
+## Syntax
+def my_fn(param):
+    print ("  ")
+    return param + 1
+# arguements 
+
+def greetings(name):
+    print(f"Hello , {name}")
+greetings()
